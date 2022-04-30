26e6699bb5505c6619102f64ec3e04b540579c19
Nutticha <nutticha30234@gmail.com>
2022-04-30 21:59:34 +0700

    up

---
+++ src/Pages/Home/home.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './home.style.scss';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>NoN MILDANDGEMJIE</h1>
+        </div>
+    )
+}
+
+export default HomePage;
